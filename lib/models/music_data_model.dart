4b44ab947b777449f4885e4fa8fe2498eff7290c
aneesmujthaba <anees@storilabs.com>
2022-11-15 13:42:55 +0530

    temp

--- lib/models/music_data_model.dart
+++ lib/models/music_data_model.dart
@@ -2,17 +2,18 @@ class MusicDataResponseModel {
   final int resultCount;
   final List<Results> results;
 
-  const MusicDataResponseModel({required this.resultCount, required this.results});
+  const MusicDataResponseModel(
+      {required this.resultCount, required this.results});
 
   factory MusicDataResponseModel.fromJson(Map<String, dynamic> json) {
     return MusicDataResponseModel(
-        resultCount: json['resultCount'],
-        results: List<Results>.from(
-        json["results"].map((x) => Results.fromJson(x))).toList(),
+      resultCount: json['resultCount'],
+      results:
+          List<Results>.from(json["results"].map((x) => Results.fromJson(x)))
+              .toList(),
     );
   }
 }
-
 
 class Results {
   final String name;
@@ -26,14 +27,14 @@ class Results {
   });
 
   factory Results.fromJson(Map<String, dynamic> json) => Results(
-    name: json["artistNname"],
-    image: json["artworkUrl100"],
-    price: json["collectionPrice"],
-  );
+        name: json["artistNname"],
+        image: json["artworkUrl100"],
+        price: json["collectionPrice"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "artistNname": name,
-    "artworkUrl100": image,
-    "collectionPrice": price,
-  };
+        "artistNname": name,
+        "artworkUrl100": image,
+        "collectionPrice": price,
+      };
 }
